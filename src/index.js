b49b31f83c412d3ed1bb3dfddc89da527ab59f65
Uladzimir Bartash <bartosh93@yandex.ru>
2021-03-09 15:04:32 +0300

    feat: first part

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  return true;
+  let boolStr, boollBracketsConfig;
+
+  let second = (bracketsConfig) => {
+    for (let i = 0; i < length.bracketsConfig; i++) {
+      if ((bracketsConfig[i][0] === "{") && (bracketsConfig[i][1] === "}")) {
+        boollBracketsConfig = true
+      } else if ((bracketsConfig[i][0] === "(") && (bracketsConfig[i][1] === ")")) {
+        boollBracketsConfig = true
+      } else if ((bracketsConfig[i][0] === "|") && (bracketsConfig[i][1] === "|")) {
+        boollBracketsConfig =  true
+      } else {
+        boollBracketsConfig =  false
+      }
+      if (boollBracketsConfig === false) return boollBracketsConfig
+    }
+    return boollBracketsConfig = true
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  if (boolStr&&bollBracketsConfig) {
+    return true
+  } else {
+    return false
+  }
 }
