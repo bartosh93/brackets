6d34958620e37ad6cbdf65b91792719cb98e2581
Uladzimir Bartash <bartosh93@yandex.ru>
2021-03-10 14:50:49 +0300

    feat: add check arrays

--- src/index.js
+++ src/index.js
@@ -17,8 +17,29 @@ module.exports = function check(str, bracketsConfig) {
     return boollBracketsConfig = true
   }
 
-
-
+////////test
+  let bracketsConfig = [['(', '}'], ['|', '|']];
+  console.log(bracketsConfig.length)
+  let second = (a) => {
+    console.log(a.length)
+    let boolStr, boollBracketsConfig;
+      for (let i = 0; i < a.length; i++) {
+        if ((a[i][0] === "{") && (a[i][1] === "}")) {
+          boollBracketsConfig = true
+        } else if ((a[i][0] === '(') && (a[i][1] === ')')) {
+          boollBracketsConfig = true
+        } else if ((a[i][0] === "|") && (a[i][1] === "|")) {
+          boollBracketsConfig =  true
+        } else {
+          boollBracketsConfig =  false
+        }
+        if (boollBracketsConfig === false) return boollBracketsConfig = false
+        console.log(boollBracketsConfig)
+      }
+      return boollBracketsConfig 
+    }
+    
+  console.log(second(bracketsConfig))
 
 
 
