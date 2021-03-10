17e7d0b8fc408801e16dadbac2c15c5c4a98154d
Uladzimir Bartash <bartosh93@yandex.ru>
2021-03-10 19:17:42 +0300

    feat: add all tests

--- src/index.js
+++ src/index.js
@@ -1,59 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  let boolStr, boollBracketsConfig;
-
-  let second = (bracketsConfig) => {
-    for (let i = 0; i < length.bracketsConfig; i++) {
-      if ((bracketsConfig[i][0] === "{") && (bracketsConfig[i][1] === "}")) {
-        boollBracketsConfig = true
-      } else if ((bracketsConfig[i][0] === "(") && (bracketsConfig[i][1] === ")")) {
-        boollBracketsConfig = true
-      } else if ((bracketsConfig[i][0] === "|") && (bracketsConfig[i][1] === "|")) {
-        boollBracketsConfig =  true
-      } else {
-        boollBracketsConfig =  false
-      }
-      if (boollBracketsConfig === false) return boollBracketsConfig
-    }
-    return boollBracketsConfig = true
+  const newArr = bracketsConfig.map(item => item.join(''))
+  for (let i = 0; i < newArr.length; i++) {
+    if (str.includes(newArr[i])) {
+      str = str.replace(newArr[i], '')
+      i = -1
+    } 
   }
-
-////////test
-  let bracketsConfig = [['(', '}'], ['|', '|']];
-  console.log(bracketsConfig.length)
-  let second = (a) => {
-    console.log(a.length)
-    let boolStr, boollBracketsConfig;
-      for (let i = 0; i < a.length; i++) {
-        if ((a[i][0] === "{") && (a[i][1] === "}")) {
-          boollBracketsConfig = true
-        } else if ((a[i][0] === '(') && (a[i][1] === ')')) {
-          boollBracketsConfig = true
-        } else if ((a[i][0] === "|") && (a[i][1] === "|")) {
-          boollBracketsConfig =  true
-        } else {
-          boollBracketsConfig =  false
-        }
-        if (boollBracketsConfig === false) return boollBracketsConfig = false
-        console.log(boollBracketsConfig)
-      }
-      return boollBracketsConfig 
-    }
-    
-  console.log(second(bracketsConfig))
-
-
-
-
-
-
-
-
-
-
-
-  if (boolStr&&bollBracketsConfig) {
-    return true
-  } else {
-    return false
-  }
+  return !str
 }
